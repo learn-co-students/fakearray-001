1a79b9032e24b5b06bb74aaad5ef8c08f46da42d
github username <github email address>
2015-10-12 18:42:31 -0400

    array

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,22 @@
 class FakeArray
 
   # you'll need a splat in this class somewhere
+  attr_accessor :nums
+  def initialize(*nums)
+    @nums = nums
+  end
 
+  def [](index)
+    self.nums[index]
+  end
+
+  def each
+    self.nums.each {|x| puts x}
+  end
+
+  def first
+    self.nums[0]
+  end
 
 
 end
