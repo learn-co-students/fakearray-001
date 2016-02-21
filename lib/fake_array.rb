9b53cbda195546e1e94a79d5900fc6a8e9c88cca
jprosario <jomi0388@gmail.com>
2016-02-21 16:58:57 -0500

    Done.

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,22 @@
 class FakeArray
 
   # you'll need a splat in this class somewhere
+  attr_accessor :arr
 
+  def initialize(*elems)
+    @arr = elems
+  end
 
+  def [](key)
+    self.arr[key]
+  end
+
+  def first
+    self.arr[0]
+  end
+
+  def each
+    self.arr.each { |e| yield(e) }
+  end
 
 end
